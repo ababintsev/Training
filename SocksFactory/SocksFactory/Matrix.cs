763640500002341ad156f71243f56b21861c48cb
Alexander Babintsev <ababintsev@higherlogic.com>
2020-08-11 00:28:08 +0300

    Matrix calculations

---
+++ SocksFactory/SocksFactory/Matrix.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocksFactory
+{
+	public static class Matrix
+	{
+		public static int MatrixDiagonals(List<List<int>> arr)
+		{			
+			int right = arr.Count - 1;
+			int result1 = 0;
+			int result2 = 0;
+
+			for (int i = 0; i < arr.Count; i++)
+			{
+				result1 += arr[i][i];
+				result2 += arr[i][right - i];				
+			}
+
+			return Math.Abs(result1 - result2);
+		}
+	}
+}
---
+++ SocksFactory/SocsFactoryTests/MatrixTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using SocksFactory;
+
+namespace SocsFactoryTests
+{
+	public class MatrixTests
+	{
+		[Test]
+		public void SumOfDiagonalsTest()
+		{
+			int n = 3;
+			List<List<int>> arr = new List<List<int>>();
+			string[] inputData = new string[]
+			{
+				"11 2 4",
+				"4 5 6",
+				"10 8 -12"
+			};
+
+			for (int i = 0; i < n; i++)
+			{
+					
+					arr.Add(inputData[i].Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+					
+			}
+			
+
+			int result = Matrix.MatrixDiagonals(arr);
+			Assert.IsTrue(result == 15);
+		}		
+	}
+}
